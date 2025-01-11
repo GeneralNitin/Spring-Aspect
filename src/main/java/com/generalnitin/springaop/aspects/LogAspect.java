75a9222c67c6286471de0d1dbc6f036430e1fea9
Alexander Kriegisch <Alexander@Kriegisch.name>
2025-01-11 12:28:24 +0700

    Simplify fetching annotation in LogAspect
    The annotation can be bound to a named advice method parameter. There is
    no need to use reflection.

--- src/main/java/com/generalnitin/springaop/aspects/LogAspect.java
+++ src/main/java/com/generalnitin/springaop/aspects/LogAspect.java
@@ -3,32 +3,23 @@ package com.generalnitin.springaop.aspects;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.reflect.MethodSignature;
-import org.springframework.beans.factory.annotation.Configurable;
-import org.springframework.stereotype.Component;
 import org.springframework.util.StopWatch;
 
-import java.lang.reflect.Method;
 import java.util.concurrent.TimeUnit;
 
 @Aspect
 public class LogAspect {
-
-    @Around("@annotation(CustomAnnotation)")
-    public Object log(ProceedingJoinPoint joinPoint) throws Throwable {
-        Method method = ((MethodSignature) joinPoint.getSignature()).getMethod();
-        CustomAnnotation execTime = method.getAnnotation(CustomAnnotation.class);
-        String metricName = execTime.metricName() != null ? execTime.metricName() : "method.timer";
-
+    @Around("@annotation(customAnnotation)")
+    public Object log(ProceedingJoinPoint joinPoint, CustomAnnotation customAnnotation) throws Throwable {
+        String metricName = customAnnotation.metricName() != null ? customAnnotation.metricName() : "method.timer";
         StopWatch stopWatch = new StopWatch();
         stopWatch.start(metricName);
-
         try {
             return joinPoint.proceed();
         }
         finally {
             stopWatch.stop();
-            System.out.println(stopWatch.lastTaskInfo().getTaskName() +" "+ stopWatch.getTotalTime(TimeUnit.MILLISECONDS));
+            System.out.println(stopWatch.lastTaskInfo().getTaskName() + " " + stopWatch.getTotalTime(TimeUnit.MILLISECONDS));
         }
     }
 }

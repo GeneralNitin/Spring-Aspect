72e5f55afb728b09bc16f430f75fd33b5347fb44
Alexander Kriegisch <Alexander@Kriegisch.name>
2025-01-11 12:07:17 +0700

    Make application call HomeService::sayHello
    so we can see "Hello World!" on the console. Also make test run the
    application, not just load the application context.

--- src/main/java/com/generalnitin/springaop/SpringAopApplication.java
+++ src/main/java/com/generalnitin/springaop/SpringAopApplication.java
@@ -1,14 +1,18 @@
 package com.generalnitin.springaop;
 
+import com.generalnitin.springaop.services.HomeService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 public class SpringAopApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringAopApplication.class, args);
+        try (ConfigurableApplicationContext context = SpringApplication.run(SpringAopApplication.class, args)) {
+            System.out.println(context.getBean(HomeService.class).sayHello());
+        }
     }
 
 }
